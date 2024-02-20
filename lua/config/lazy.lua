4aaa3023ccc8596b3492d7e3aebf8cf712fc4b61
Anthony Hicks <24363760+anthony-hicks@users.noreply.github.com>
2024-02-20 15:12:41 +0000

    feat(lsp): enable clangd, cmake, and dap extras

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -11,6 +11,9 @@ require("lazy").setup({
     -- add LazyVim and import its plugins
     { "LazyVim/LazyVim", import = "lazyvim.plugins" },
     -- import any extras modules here
+    { import = "lazyvim.plugins.extras.lang.clangd" },
+    { import = "lazyvim.plugins.extras.lang.cmake" },
+    { import = "lazyvim.plugins.extras.dap.core" },
     -- { import = "lazyvim.plugins.extras.lang.typescript" },
     -- { import = "lazyvim.plugins.extras.lang.json" },
     -- { import = "lazyvim.plugins.extras.ui.mini-animate" },
