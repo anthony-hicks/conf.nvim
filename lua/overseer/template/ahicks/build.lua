6dbb49fde556c1787cee59966034ba82c24c4e42
Anthony Hicks <24363760+anthony-hicks@users.noreply.github.com>
2024-02-20 22:47:43 +0000

    fix(overseer): disable term for build
    So programs don't send color output.

--- lua/overseer/template/ahicks/build.lua
+++ lua/overseer/template/ahicks/build.lua
@@ -1,42 +1,46 @@
 ---@type overseer.TemplateDefinition
 return {
-    name = "Build Project",
-    params = {
-      build_type = {
-        name = "Build Type",
-        type = "enum",
-        choices = { "Debug", "Release" },
-      },
+  name = "Build Project",
+  params = {
+    build_type = {
+      name = "Build Type",
+      type = "enum",
+      choices = { "Debug", "Release" },
     },
-    builder = function(params)
-      local args = { "-j" }
+  },
+  builder = function(params)
+    local args = { "-j" }
 
-      if params.build_type == "Debug" then
-        table.insert(args, "DEBUG=1")
-      end
+    if params.build_type == "Debug" then
+      table.insert(args, "DEBUG=1")
+    end
 
-      ---@type overseer.TaskDefinition
-      return {
-        cmd = { "make" },
-        args = args,
-        components = {
-          "default",
-          {
-            "ahicks.on_start_close_trouble",
-          },
-          {
-            "on_output_quickfix",
-            open = true,
-            open_height = 12,
-          },
-          {
-            "ahicks.quickfix",
-            close = "always",
-          },
-          {
-            "ahicks.on_failure_open_trouble",
-          },
+    ---@type overseer.TaskDefinition
+    return {
+      cmd = { "make" },
+      args = args,
+      strategy = {
+        "jobstart",
+        use_terminal = false,
+      },
+      components = {
+        "default",
+        {
+          "ahicks.on_start_close_trouble",
         },
-      }
-    end,
-  }
+        {
+          "on_output_quickfix",
+          open = true,
+          open_height = 12,
+        },
+        {
+          "ahicks.quickfix",
+          close = "always",
+        },
+        {
+          "ahicks.on_failure_open_trouble",
+        },
+      },
+    }
+  end,
+}
