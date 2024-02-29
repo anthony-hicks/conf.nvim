92bc0d4cb677b1d8804a0d976437637555b0dcc7
Anthony Hicks <24363760+anthony-hicks@users.noreply.github.com>
2024-02-29 21:47:30 +0000

    feat(overseer): arbitrary build args

--- lua/overseer/template/ahicks/build.lua
+++ lua/overseer/template/ahicks/build.lua
@@ -13,6 +13,10 @@ return {
       choices = { "ASan", "UBSan", "TSan" },
       optional = true,
     },
+    args = {
+      name = "Args",
+      optional = true,
+    },
   },
   builder = function(params)
     local args = { "-j" }
@@ -23,6 +27,10 @@ return {
 
     if params.sanitizer then
       table.insert(args, string.upper(params.sanitizer) .. "=1")
+    end
+
+    if params.args then
+      table.insert(args, params.args)
     end
 
     ---@type overseer.TaskDefinition
@@ -44,7 +52,7 @@ return {
           "ahicks.trouble",
           close_on_start = true,
           open_on_status = "failure",
-          open_first_item = true,
+          open_first_item = false, -- Temporary until we fix the categorization problem
         },
         {
           "ahicks.quickfix",
