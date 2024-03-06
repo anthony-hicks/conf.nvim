95a8dd0bb3475995ed502c1d2ef90ce02f8afe5a
Anthony Hicks <24363760+anthony-hicks@users.noreply.github.com>
2024-03-06 22:31:08 +0000

    fix(telescope): use find_files
    Instead of git_files for <leader>fF. <leader>fg opens git_files
    so I don't see a reason to exclude find_files.

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -28,5 +28,14 @@ return {
       end,
       desc = "[S]earch [S]ymbols (workspace)",
     },
+    -- When I delete a file, I don't want it to show up in Find Files. LazyVim
+    -- uses git_files for this keymap by default, which still shows the deleted
+    -- files because they are tracked by Git. But now that we have <leader>fg,
+    -- there's no reason to use git_files elsewhere, I'll use it when I want to.
+    {
+      "<leader>fF",
+      "<cmd>Telescope find_files<CR>",
+      desc = "[F]ind [F]iles",
+    },
   },
 }
