040df3b62635447984c2ec3a2fee8ad02ff966aa
Anthony Hicks <24363760+anthony-hicks@users.noreply.github.com>
2024-06-20 13:35:22 +0000

    fix: disable flash
    I couldn't search for some full terms due to this plugin, and
    I never used it anyways.

---
+++ lua/plugins/disabled.lua
@@ -0,0 +1,5 @@
+return {
+    -- disable flash, it made it hard to search for some things because the hotkey
+    -- to jump to a search result was the same key I wanted to search next
+    { "folke/flash.nvim", enabled = false },
+  }
