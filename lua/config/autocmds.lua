784ebbd6ae80350d7d7ed5da6b347980b9a0fc01
Anthony Hicks <anthony-hicks@github.com>
2024-08-23 10:07:38 -0500

    feat(luasnip): disable <tab> jumping back to prev
    If I "exited" a snippet without tabbing through all of the
    placeholders, later on, if I pressed <tab> again it would
    jump me back to the "next" placeholder.
    
    I made this awhile back and I'm not sure if it's still needed, but
    it might as well be tracked.

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -9,3 +9,25 @@ vim.api.nvim_create_autocmd({ "FileType" }, {
     vim.b.autoformat = false
   end,
 })
+
+-- Disable <tab> jumping back to previous snippets
+-- after we've already left insert mode
+local luasnip = require("luasnip")
+
+local unlinkgrp =
+  vim.api.nvim_create_augroup("UnlinkSnippetOnModeChange", { clear = true })
+
+vim.api.nvim_create_autocmd("ModeChanged", {
+  group = unlinkgrp,
+  pattern = { "s:n", "i:*" },
+  desc = "Forget the current snippet when leaving the insert mode",
+  callback = function(evt)
+    if
+      luasnip.session
+      and luasnip.session.current_nodes[evt.buf]
+      and not luasnip.session.jump_active
+    then
+      luasnip.unlink_current()
+    end
+  end,
+})
