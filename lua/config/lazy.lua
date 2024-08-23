66962e0ca20f68d5d3165511498ed3cba61f0f9f
Anthony Hicks <anthony-hicks@github.com>
2024-08-23 10:08:51 -0500

    feat(dap): remove dap config
    Fun to play with, but never fully got it working well.

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -12,8 +12,6 @@ require("lazy").setup({
     { "LazyVim/LazyVim", import = "lazyvim.plugins" },
     -- import any extras modules here
     { import = "lazyvim.plugins.extras.lang.clangd" },
-    { import = "lazyvim.plugins.extras.lang.cmake" },
-    { import = "lazyvim.plugins.extras.dap.core" },
     -- { import = "lazyvim.plugins.extras.lang.typescript" },
     -- { import = "lazyvim.plugins.extras.lang.json" },
     -- { import = "lazyvim.plugins.extras.ui.mini-animate" },
