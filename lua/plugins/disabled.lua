8fc786f809bcf65148e1e17613758836583fefac
Anthony Hicks <anthony-hicks@github.com>
2025-05-03 16:51:37 -0500

    update lazyvim

--- lua/plugins/disabled.lua
+++ lua/plugins/disabled.lua
@@ -1,5 +1,6 @@
 return {
-    -- disable flash, it made it hard to search for some things because the hotkey
-    -- to jump to a search result was the same key I wanted to search next
-    { "folke/flash.nvim", enabled = false },
-  }
+  -- disable flash, it made it hard to search for some things because the hotkey
+  -- to jump to a search result was the same key I wanted to search next
+  { "folke/flash.nvim", enabled = false },
+  { "folke/noice.nvim", enabled = false },
+}
